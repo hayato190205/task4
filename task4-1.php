3392afb929d5dc99d818609f1a1c1b9d7a68c5fb
hayato190205 <hayato190205@gmail.com>
2015-12-13 05:37:42 +0000

    first commit

---
+++ task4-1.php
@@ -0,0 +1,25 @@
+<?php
+
+// ある正の整数a と b が与えられたとき、a と b の大きさを比較して、大きい方の値を出力せよ。ただし、a と b が同じ場合は "入力された値は等しいです" と出力せよ。
+// [実行結果例]
+// a = 5, b = 3 のとき
+// 5
+// a = 2, b = 2 のとき
+// 入力された値は等しいです
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>数の比較</title>
+</head>
+<body>
+  <form action="task4-2.php" method="post">
+    a <input type="text" name="a">
+    b <input type="text" name="b">
+    <input type="submit" value="入力">
+  </form>
+</body>
+</html>
